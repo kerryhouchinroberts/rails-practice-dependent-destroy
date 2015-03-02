ac3cb39fd0a42517be1cc5323da32a89cef966dd
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-03-02 13:10:22 -0700

    Add dependent destroy to medications under patients and add delete patient link on index

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,4 +2,13 @@ class PatientsController < ApplicationController
   def show
     @patient = Patient.find(params[:id])
   end
+
+  def destroy
+    @patient = Patient.find(params[:id])
+    if
+      @patient.destroy
+      redirect_to root_path, notice: "Patient successfully destroyed"
+    end
+  end
+
 end
